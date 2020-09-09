8bf35b7c3634bf525593d99bdfa235da3b55341a
Amanda Ciobanu <amanda@ciobanu.org>
2020-09-09 13:47:18 +0100

    feat: expense component

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -3,7 +3,20 @@
  export let amount = 0;
 </script>
 
-<div>
-  <h2>{name}</h2>
-  <h2>{amount}</h2>
-</div>
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>{name} <button class="amount-btn">
+      <i class="fas fa-caret-down"></i>
+    </button>
+    </h2>
+    <h4>Amount: ${amount}</h4>
+  </div>
+  <div class="expense-button">
+    <button class="expense-btn edit-btn">
+      <i class="fas fa-pen"></i>
+    </button>
+    <button class="expense-btn delete-btn">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>
+</article>
