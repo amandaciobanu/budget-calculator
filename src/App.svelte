f60cf427161cd58cae1f0217e89e62e4dc6ca098
Amanda Ciobanu <amanda@ciobanu.org>
2020-09-09 16:40:07 +0100

    feat: add expense form html structure

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   import Navbar from './Navbar.svelte';
   import ExpensesList from './ExpensesList.svelte';
   import Totals from './Totals.svelte';
+  import ExpenseForm from './ExpenseForm.svelte';
   
   //data
   import expensesData from './expenses';
@@ -36,6 +37,7 @@
 <!--HTML-->
 <Navbar />
 <main class="content">
+  <ExpenseForm />
   <Totals title="total expenses"  {total}/>
   <ExpensesList {expenses} />
   <button type="button" class="btn btn btn-primary btn-block" on:click={clearExpenses}>
